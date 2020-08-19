fca4ac2a13e1e1077e0c284aa1356dffec251cd6
dev-kshitij-sawant <59193661+dev-kshitij-sawant@users.noreply.github.com>
2020-08-19 19:21:53 +0530

    Update wp-config.php
    change database details

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'netverk' );
+define( 'DB_NAME', 'db_netverk' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'netverkadmin' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'netverk@2020#' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'pixcelco' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
